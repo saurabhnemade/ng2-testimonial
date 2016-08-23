62432775be50900b5f348a9dcd957b4b083aa79c
Oss Martin <ossmartin@gmail.com>
2016-08-23 13:50:13 +0200

    Add initial package.json

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Let moulde say hello to you.
+ *
+ * @param  {String} name
+ * @return {String}
+ */
+module.exports = {
+  sayHello: function(name) {
+    return 'hello ' + name; 
+  }
+};
