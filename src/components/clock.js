4cee66c5881ab305f2d44b3606c209fc9168af78
Martin Oss <ossmartin@gmail.com>
2016-08-31 11:56:04 +0200

    Update gitignore

--- src/components/clock.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var clock_component_1 = require('./clock/clock.component');
-var clock_component_2 = require('./clock/clock.component');
-exports.ClockComponent = clock_component_2.ClockComponent;
-exports.CLOCK_DIRECTIVES = [clock_component_1.ClockComponent];
-//# sourceMappingURL=clock.js.map
--- src/components/timer/timer.service.spec.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=timer.service.spec.js.map
